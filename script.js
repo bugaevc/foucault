5f365c1c848b9864a5ad5e01fcf9daf5b79e1209
Sergey Bugaev <bugaevc@gmail.com>
2016-05-03 12:31:03 +0300

    Add some basic rotation

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHei
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild( renderer.domElement );
+document.body.appendChild(renderer.domElement);
 
 var light = new THREE.PointLight(0xffffff, 1, 0);
 light.position.set(5, 5, 5);
@@ -14,7 +14,7 @@ scene.add(light);
 
 var geometry = new THREE.SphereGeometry(2, 32, 32);
 
-var earthMesh;
+var earthMesh = null;
 var textureUrl = "//raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/land_ocean_ice_cloud_2048.jpg";
 var loader = new THREE.TextureLoader();
 loader.crossOrigin = '';
@@ -28,7 +28,8 @@ camera.position.z = 5;
 
 function render() {
 	requestAnimationFrame(render);
-
+	if (earthMesh != null)
+		earthMesh.rotateX(0.001);
 	renderer.render(scene, camera);
 }
 render();
